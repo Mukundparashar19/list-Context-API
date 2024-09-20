b031928b698f74361fb4f4c5dd60be8140d22b81
Mukund Parashar <mukundparashar004@gmail.com>
2024-09-20 23:51:39 +0530

    Updated : files

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodoForm() {
+  return (
+    <div>
+      <div>todo form</div>
+    </div>
+  )
+}
+
+export default TodoForm
---
+++ src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    //Properties
+    todos:[
+        {
+            id: 1,
+            todo: 'Todo msg',
+            completed: false,
+        }
+    ],
+    //functionality we will decide what this fx will do in our app.jsx or some ware else
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) = {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const usetodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
